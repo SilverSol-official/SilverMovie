7808aecf63ff81059f4d303d1ff6dd1290b986fa
SilverSol <Silversol3301@gmail.com>
2023-03-13 00:39:40 +0200

    last last final

--- Components/popUp.tsx
+++ Components/popUp.tsx
@@ -134,7 +134,7 @@ const PopUp:FC<PopUpPropsType> = ({ id}) => {
               <div className="img">
                 <CardMedia
                   component="img"
-                  sx={{ mt: 5, width: "140px" }}
+                  sx={{ mt: 5, width: "100px", height:'200px' }}
                   image={movieResult.Poster}
                   alt="green iguana"
                 />
